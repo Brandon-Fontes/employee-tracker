21f59c21e4f6debc102b7c64f773058fc79a790e
DaFonz <brandonjfontes@gmail.com>
2021-02-25 17:30:55 -0700

    check-point

--- employees_db.sql
+++ employees_db.sql
@@ -9,14 +9,39 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE department (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
     PRIMARY KEY (id)
 );
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Human Resources"), ("Legal"), ("Finance"), ("Artist");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", "100000", "7"), ("Software Developer", "70000", "2"), ("Lawyer", "60000", "3"), ("Lawyer", "60000", "4"), ("Actuary", "60000", "5"), ("Artist", "70000", "6"), ("Salesman", "40000", "1");
+
+INSERT INTO employee (fist_name, last_name, role_id, manager_id)
+VALUES ("Elon", "Musk", "4"), ("Mark", "Rober", "3", "4"), ("Michael", "Reeves", "4", "7");
+
+SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
+
+Select r.id, r.title, r.salary, d.name AS Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id;
+
+Select id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee;
+
+UPDATE employee SET role_id = 3 WHERE id = 8;
+UPDATE employee SET ? WHERE ?;
+
+DELETE FROM department WHERE id = 13;
